8b7a56d4db13e5f9e921013358e077e22c8c5ad4
Patrick Varin <patrickjvarin@gmail.com>
2017-11-02 09:06:18 -0400

    modify parser to collect data from state and control variables, and also keep around the time variable. Note that if the end time is equal to 4 or 6 seconds, then the trajectory might not be energetically optimal because the timelimit constraint could be active

--- gen_data/acrobot/stitch_data.py
+++ gen_data/acrobot/stitch_data.py
@@ -3,20 +3,25 @@ import numpy as np
 import json
 import os
 
-# load filenames
-path = "./data"
-prefix = "acrobot_trajectory_"
-filenames = [os.path.join(path,f) for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and prefix in f]
+def stitch_data(path,prefix,datafile):
+	# load filenames
+	filenames = [os.path.join(path,f) for f in os.listdir(path) if os.path.isfile(os.path.join(path,f)) and prefix in f]
 
-# initialize dataset
-traj = np.loadtxt(filenames[0],dtype=np.float64,delimiter=',')
-Nx, Nt = traj.shape
-N = len(filenames)
-dataset = np.zeros((N,Nx,Nt),dtype=np.float64)
+	# initialize dataset
+	traj = np.loadtxt(filenames[0],dtype=np.float64,delimiter=',',ndmin=2)
+	Nx, Nt = traj.shape
+	N = len(filenames)
+	dataset = np.zeros((N,Nx,Nt),dtype=np.float64)
 
-# populate dataset
-for i,f in enumerate(filenames):
-	dataset[i,:,:] = np.loadtxt(f,dtype=np.float64,delimiter=',')
+	# populate dataset
+	for i,f in enumerate(filenames):
+		dataset[i,:,:] = np.loadtxt(f,dtype=np.float64,delimiter=',')
 
-datafile = "acrobot_trajectory"
-np.save(datafile,dataset) 
+	# save dataset
+	np.save(datafile,dataset)
+
+if __name__ =='__main__':
+	# parse the control, time, and state data
+	stitch_data("./data","acrobot_trajectory_time_","acrobot_trajectory_time")
+	stitch_data("./data","acrobot_trajectory_control_","acrobot_trajectory_control")
+	stitch_data("./data","acrobot_trajectory_state_","acrobot_trajectory_state")
