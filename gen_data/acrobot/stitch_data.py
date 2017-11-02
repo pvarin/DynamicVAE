d0db21a6500a4cf3681b3f2e14d09dc29c27a5b3
Patrick Varin <patrickjvarin@gmail.com>
2017-11-02 08:19:20 -0400

    put the data into a better format

--- gen_data/acrobot/stitch_data.py
+++ gen_data/acrobot/stitch_data.py
@@ -12,15 +12,11 @@ filenames = [os.path.join(path,f) for f in os.listdir(path) if os.path.isfile(os
 traj = np.loadtxt(filenames[0],dtype=np.float64,delimiter=',')
 Nx, Nt = traj.shape
 N = len(filenames)
-dataset = np.zeros(N,dtype="int32, " + str((Nx,Nt)) + "float64")
+dataset = np.zeros((N,Nx,Nt),dtype=np.float64)
 
 # populate dataset
 for i,f in enumerate(filenames):
-	idx = range(i*4,(i+1)*4)
-	dataset[i][0] = i
-	# print dataset[i][1]
-	# print np.loadtxt(f,dtype=np.float64,delimiter=',')
-	dataset[i][1] = np.loadtxt(f,dtype=np.float64,delimiter=',')
+	dataset[i,:,:] = np.loadtxt(f,dtype=np.float64,delimiter=',')
 
 datafile = "acrobot_trajectory"
 np.save(datafile,dataset) 
